4f738982ad3772d54ba56bce800a7d59fc4d5f0f
Timothy Meixell <misternu@gmail.com>
2017-06-29 16:40:01 -0500

    Add react

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,41 @@
+/* eslint no-console:0 */
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+//
+// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
+// layout file, like app/views/layouts/application.html.erb
+
+// console.log('Hello World from Webpacker')
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('root')
+  );
+})
+
+// const Hello = props => (
+//   <div>Hello {props.name}!</div>
+// )
+
+// Hello.defaultProps = {
+//   name: 'David'
+// }
+
+// Hello.propTypes = {
+//   name: PropTypes.string
+// }
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   let string = foobar();
+//   ReactDOM.render(
+//     <Hello name={string} />,
+//     document.body.appendChild(document.createElement('div')),
+//   )
+// })
